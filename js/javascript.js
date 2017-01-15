e8694361360a5241155942f3d9a5d61aba9fa21d
shadsluiter <shadsluiter@shads-MacBook-Air.local>
2017-01-15 15:32:34 -0700

    first

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,55 @@
 $(document).ready(function() {
-	var $citylist = $('#city-list');
-	var citytemplate = $('#city-template').html();
 
-$('#citybutton').click(function() {
+	var template = $('#template').html();
 
-	var cityname = $('#cityname').val();
-	$('#cityname').html("").focus().select();
-	$citylist.html("Searching...")
+	$('#artistname').focus();
+
+	$('#getapibutton').click(function(event) {
+		event.preventDefault();
+
+		var artistname = $('#artistname').val();
+		var songtitle = $('#songtitle').val();
+
+		$('#songtitle').html("");
+		$('#artistname').html("").focus().select();
+
+
+		$('#artistplacement').html("Searching...")
 
 
 
+			getLyrics = function() { $.ajax({
+				url  : 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/matcher.lyrics.get?q_artist='+ artistname + '&q_track=' + songtitle,
+			 type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+		 data: {}, // Additional parameters here
+		 dataType: 'json',
+		 success: function(data) {
 
-		$.getJSON( {
-    url  : 'https://maps.googleapis.com/maps/api/geocode/json',
-    data : {
-        sensor  : false,
-        address : cityname
-    },
-    success : function( data, textStatus ) {
-        console.log( textStatus, data );
-			//	$citylist.append(data.results[0].formatted_address);
-			$citylist.html(Mustache.render(citytemplate,data.results[0]));
-    }
-		});
+			 //
+				 //Change data.source to data.something , where something is whichever part of the object you want returned.
+				 //To see the whole object you can output it to your browser console using:
+				 console.log(data.lyrics_body);
 
+				 //$output.addClass('white-space-pre');
+				  // $output.html(Mustache.render(theTemplate,data);
+				 //var dataObj = data;
+				 //$output.html(Mustache.render(theTemplate,data));
+				 $('#songplacement').html(data.lyrics_body).addClass('white-space-pre');
+
+				 $('#artistplacement').html(artistname + ' ' + songtitle);
+				 },
+		 error: function(err) {
+			 $('#artistplacement').html('Sorry, I did not find anything. ')
+				$('#songplacement').html('Please try again.');
+			 artist.select.focus();
+
+	 },
+		 beforeSend: function(xhr) {
+		 xhr.setRequestHeader("X-Mashape-Authorization", "hW0RuXxRGimshPuBnZss7FBcF3QTp15W1nrjsn8sUDbdHYanZx"); // Enter here your Mashape key
+		 }
+ });
+}
+
+		getLyrics();
 	});
 });
